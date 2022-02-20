50425802d6c7a32156821ae46c5804c42b67ac79
Alex Kuznetsov <a.kuznetsov23@gmail.com>
2022-02-20 16:43:35 +0200

    Внес исправления с учетом правок

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 const square = document.getElementById('square');
-const text = document.getElementById('text');
+const inputText = document.getElementById('text');
 const btn = document.getElementById('btn');
 
 const elem = document.getElementById('e_btn');
 
 const range = document.getElementById('range');
 const circle = document.getElementById('circle');
+let elemspan = document.getElementById('range-span');
 
+elemspan.textContent = '50%';
 
-const logger = function (event) {
+const changeColor = function (event) {
+      square.style.backgroundColor = inputText.value;
+};
+
+const rangeChanging = function (event) {
       circle.style.width = range.value + '%';
       circle.style.height = range.value + '%';
-}
+      elemspan.textContent = range.value + '%';
+};
 
-btn.addEventListener('click', function() {
-      if (text.value === "yellow") {
-            square.style.backgroundColor = 'yellow';
-      } else if (text.value === "red") {
-            square.style.backgroundColor = 'red';
-      } else if (text.value === "blue") {
-            square.style.backgroundColor = 'blue';
-      } else if (text.value === "green") {
-            square.style.backgroundColor = 'green';
-      }
-});
+btn.addEventListener('click', changeColor);
 
 elem.remove();
 
-
-range.addEventListener('input', logger);
+range.addEventListener('input', rangeChanging);
 
 
